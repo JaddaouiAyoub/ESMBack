560b780cb9c0c46566e638f5257d1d1118c9c5df
JaddaouiAyoub <jaddaoui.salim@gmail.com>
2025-05-04 20:38:01 +0100

    allowed origins change

--- src/main/java/org/jad/auth/config/SecurityConfiguration.java
+++ src/main/java/org/jad/auth/config/SecurityConfiguration.java
@@ -27,6 +27,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -94,7 +96,7 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("*");
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://esm-aj.vercel.app"));
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
